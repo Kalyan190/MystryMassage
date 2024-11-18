281b1893d2d70a7e1018d8711c02bd8f65f0c3c9
Kalyan Kumar <kalyaniit795@gmail.com>
2024-11-19 02:14:13 +0530

    Signup backend code write

--- src/lib/DbConnect.ts
+++ src/lib/DbConnect.ts
@@ -14,8 +14,16 @@ async function DbConnect() : Promise<void>{
 
    try {
       const db = await mongoose.connect(process.env.MONGODB_URI || '',{})
+      Connection.isConnected = db.connections[0].readyState
+
+      console.log("DB connected Successfully!")
       
    } catch (error) {
-      
+     console.log("Database Connection Failed",error)
+
+
+      process.exit(1);
    }
 }
+
+export default DbConnect;
